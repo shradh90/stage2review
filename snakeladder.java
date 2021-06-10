2c933c9a7aa69d674a94909a1bdb1a5df2e905b6
shradh90 <shradhad1996@gmail.com>
2021-06-10 14:31:58 +0530

    snake ladder game with single player

---
+++ snakeladder.java
@@ -0,0 +1,9 @@
+public class snakeladder {
+	public static void main(String[] args) {
+		System.out.println("**************************************************************\t");
+		System.out.println("welcome to snake and ladder game");
+		System.out.println("**************************************************************");
+		int player1;
+		System.out.println("snake and ladder with single player at start position 0");
+	}
+}
