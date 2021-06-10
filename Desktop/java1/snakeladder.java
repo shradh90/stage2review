c3fd2eeb66fc201bd5dfdf79c68d240c9e73c558
shradh90 <shradhad1996@gmail.com>
2021-06-10 15:08:14 +0530

    snake ladder with single person and start at pos 0

---
+++ Desktop/java1/snakeladder.java
@@ -0,0 +1,10 @@
+public class snakeladder {
+	public static void main(String[] args) {
+		System.out.println("**************************************************************\t");
+		System.out.println("welcome to snake and ladder game");
+		System.out.println("**************************************************************");
+		int player1;
+		int pos = 0;
+		System.out.println("snake and ladder with single player at start position 0");
+	}
+}
